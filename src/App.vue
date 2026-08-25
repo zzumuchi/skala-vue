854bf5141adeb90d6c9aaef07d13171881464f32
zzumuchi <jykim0418@gmail.com>
2026-08-25 09:15:34 +0900

    feat(day2): 전날 내용 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <script setup>
-// 1. 학습환경 구성
-import SampleOne from './components/practices/basic/SampleOne.vue'
-import SampleTwo from './components/practices/basic/SampleTwo.vue'
+/* 1. 학습환경 구성 */
+// Reactiviy Example
+import SampleOne from './components/practices/day1/1st/SampleOne.vue'
+// Text Interpolation Example
+import SampleTwo from './components/practices/day1/1st/SampleTwo.vue'
+
+/* 2. Vue Directive */
+// v-html / v-html XSS / v-text
+import VueHtml from './components/practices/day1/2nd/VueHtml.vue'
+import VueHtmlXss from './components/practices/day1/2nd/VueHtmlXss.vue'
+import VueText from './components/practices/day1/2nd/VueText.vue'
+// v-bind
+import VueBind from './components/practices/day1/2nd/VueBind.vue'
+import VueBindClass from './components/practices/day1/2nd/VueBindClass.vue'
+import VueBindStyle from './components/practices/day1/2nd/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/day1/2nd/VueBindShorthand.vue'
+// v-if / v-else-if / v-else / v-show
+import VueIf from './components/practices/day1/2nd/VueIf.vue'
+import VueShow from './components/practices/day1/2nd/VueShow.vue'
+// v-for
+import VueFor from './components/practices/day1/2nd/VueFor.vue'
+// v-pre / v-cloak / v-once / v-memo
+import VuePre from './components/practices/day1/2nd/VuePre.vue'
+import VueCloak from './components/practices/day1/2nd/VueCloak.vue'
+import VueOnce from './components/practices/day1/2nd/VueOnce.vue'
+import VueMemo from './components/practices/day1/2nd/VueMemo.vue'
 
 </script>
 
 <template>
   <div class="practice-container">
-    <h1>1. 학습환경구성</h1>
+    <h1>1. 학습환경 구성</h1>
     <hr />
     <SampleOne />
     <SampleTwo />
+    <h1>2. Vue Directive</h1>
+    <hr />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <VueIf />
+    <VueShow />
+    <VueFor />
+    <VuePre />
+    <VueCloak />
+    <VueOnce />
+    <VueMemo />
   </div>
 </template>
 
